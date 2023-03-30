8da5b097a1cfc1957c7cb4f1334e8ab6df0f10fc
Yoo-Nyx <nishaujjwal1989@gmail.com>
2023-03-30 22:28:41 +0530

    Staff-Details CRUD

--- controllers/staff.routes.js
+++ controllers/staff.routes.js
@@ -19,4 +19,76 @@ router.post("/add-staff", async (req, res) => {
   }
 });
 
+router.post("/edit-staff", async (req, res) => {
+  try {
+    const result = await StaffService.edit_staff(req.body);
+    const response = {};
+    response.data = result;
+    response.success = true;
+    response.datetime = new Date();
+    res.send(response);
+  } catch (err) {
+    console.log(err);
+    const response = {};
+    response.data = err.message;
+    response.success = false;
+    response.datetime = new Date();
+    res.status(500).send(response);
+  }
+});
+
+router.get("/get-staff", async (req, res) => {
+  try {
+    const result = await StaffService.get_staff(req.body);
+    const response = {};
+    response.data = result;
+    response.success = true;
+    response.datetime = new Date();
+    res.send(response);
+  } catch (err) {
+    console.log(err);
+    const response = {};
+    response.data = err.message;
+    response.success = false;
+    response.datetime = new Date();
+    res.status(500).send(response);
+  }
+});
+
+router.get("/get-all-staff", async (req, res) => {
+  try {
+    const result = await StaffService.get_all_staff();
+    const response = {};
+    response.data = result;
+    response.success = true;
+    response.datetime = new Date();
+    res.send(response);
+  } catch (err) {
+    console.log(err);
+    const response = {};
+    response.data = err.message;
+    response.success = false;
+    response.datetime = new Date();
+    res.status(500).send(response);
+  }
+});
+
+router.get("/delete-staff", async (req, res) => {
+  try {
+    const result = await StaffService.delete_staff(req.body);
+    const response = {};
+    response.data = result;
+    response.success = true;
+    response.datetime = new Date();
+    res.send(response);
+  } catch (err) {
+    console.log(err);
+    const response = {};
+    response.data = err.message;
+    response.success = false;
+    response.datetime = new Date();
+    res.status(500).send(response);
+  }
+});
+
 module.exports = router;
--- services/staff.service.js
+++ services/staff.service.js
@@ -7,6 +7,36 @@ class StaffService {
     let res = await db.query(query, values);
     return `Inserted Data with ID: ${res.rows[0].staff_id}`;
   }
+
+  async edit_staff(params) {
+    const values = Object.values(params.data);
+    const query = `UPDATE staff_details SET fname=$1,mname=$2,lname=$3,address=$4,account_no=$5,bank_name=$6,bank_branch=$7,ifsc_code=$8 WHERE staff_id=${params.staff_id}  AND deleted=0`;
+    let res = await db.query(query, values);
+    console.log(res);
+    return `Staff Details Updated! Number of Rows Changed ${res.rowCount}`;
+  }
+
+  async get_staff(params) {
+    const query = `SELECT * FROM staff_details WHERE staff_id=${params.staff_id} AND deleted=0`;
+    let res = await db.query(query);
+    console.log(res.rows);
+    return res.rows;
+  }
+
+  async get_all_staff() {
+    const query = `SELECT * FROM staff_details WHERE deleted=0`;
+    let res = await db.query(query);
+    console.log(res.rows);
+    return res.rows;
+  }
+
+  async delete_staff(params) {
+    const query = `UPDATE staff_details SET deleted=1 WHERE staff_id=${params.staff_id}`;
+    let res = await db.query(query);
+    res = await this.get_all_staff();
+    console.log(res);
+    return res;
+  }
 }
 
 module.exports = new StaffService();
